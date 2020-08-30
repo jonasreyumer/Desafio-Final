0bdcb8e2e76c28e1a59ea8994bba6edff8b0adfa
Joaco Esteban <dev.joaco.esteban@gmail.com>
2020-08-30 16:55:52 -0300

    IMPROVE, manipulating dom

--- js/main.js
+++ js/main.js
@@ -1,67 +1,108 @@
 // RENUNCIA
-  // ---Devuelve---
-  // Aguinaldo proporcional
-  // Vaciaciones no gozadas
-  // Aguinaldo / Vacaciones
-  // ---Params---
-  // Dias trabajados
-  // Meses trabajados TOTAL
-  // function agregartxt1(){                
-  //   let text = document.createTextNode(". Esto es un nuevo parrafo agregado con appendChild()");                                    
-  //   document.getElementById("stepLabel1").appendChild(text); 
-  //   } 
+// ---Devuelve---
+// Aguinaldo proporcional
+// Vaciaciones no gozadas
+// Aguinaldo / Vacaciones
+// ---Params---
+// Dias trabajados
+// Meses trabajados TOTAL
+// function agregartxt1(){                
+//   let text = document.createTextNode(". Esto es un nuevo parrafo agregado con appendChild()");                                    
+//   document.getElementById("stepLabel1").appendChild(text); 
+//   } 
 
-  let formToSend = document.getElementById('form1');
-        formToSend.addEventListener('submit', function(event) {
-            event.preventDefault();
-            let title = event.target.elements.firstD.value
-            console.log(title)
-            function agregartxt1(){
-              document.getElementById("sLabel1").appendChild(title);
-            }  
-        })
+function fnAguinaldoProporcional(salary, semesterWorkingDays) {
+  return salary / 360 * semesterWorkingDays;
+}
+
+function fnVacacionesNoGozadas(salary, vacationDays, yearWorkingDays) {
+  return (salary / 25 * vacationDays) / 360 * yearWorkingDays;
+}
+
+function fnIntegracionMesDespido(salary, monthWorkingDays) {
+  return (salary / 30 * (30 - monthWorkingDays));
+}
 
 
+function resignationLiquidation(yearWorkingDays, semesterWorkingDays, vacationDays, salari) {
+  const aguinaldoProporcional = fnAguinaldoProporcional(salari, semesterWorkingDays);
+  const vacacionesNoGozadas = fnVacacionesNoGozadas(salari, vacationDays, yearWorkingDays);
+  const aguinaldoOverVacaciones = vacacionesNoGozadas / 12;
 
-  function fnAguinaldoProporcional (salary, semesterWorkingDays) {
-    return salary / 360 * semesterWorkingDays;
+  return {
+    resultAsObj: {
+      aguinaldoProporcional: aguinaldoProporcional,
+      vacacionesNoGozadas: vacacionesNoGozadas,
+      aguinaldoOverVacaciones: aguinaldoOverVacaciones
+    },
+    resultAsArr: [
+      aguinaldoProporcional,
+      vacacionesNoGozadas,
+      aguinaldoOverVacaciones
+    ]
   }
-  
-  function fnVacacionesNoGozadas (salary, vacationDays, yearWorkingDays) {
-    return (salary / 25 * vacationDays) / 360 * yearWorkingDays;
+}
+
+function fireLiquidation(yearWorkingDays, semesterWorkingDays, vacationDays, salari, monthWorkingDays) {
+  const aguinaldoProporcional = fnAguinaldoProporcional(salari, semesterWorkingDays);
+  const vacacionesNoGozadas = fnVacacionesNoGozadas(salari, vacationDays, yearWorkingDays);
+  const aguinaldoOverVacaciones = vacacionesNoGozadas / 12;
+  const integracionMesDespido = fnIntegracionMesDespido(salari, monthWorkingDays);
+  const mesPreaviso = fnMesPreaviso(salari, monthWorkingDays)
+}
+
+// ------
+
+function onSubmit (event) {
+  event.preventDefault()
+  if (currentStep > totalSteps) return
+  // buscar valor
+  const value = document.getElementById('step-' + currentStep + '-input').value
+
+  userInput[currentStep - 1] = value
+
+
+  const element = document.getElementById('step-' + currentStep + '-info')
+  console.log(element)
+  element.innerText = userInput[currentStep - 1]
+
+
+  currentStep += 1
+  updateInputVisibiliy()
+
+  if (currentStep > totalSteps) onFinalSubmit()
+}
+
+
+function updateInputVisibiliy () {
+  for (let i = 1; i <= totalSteps; i++) {
+    let functionName
+
+    if (i === currentStep) functionName = 'remove'
+    else functionName = 'add'
+
+    document.getElementById('step-' + i).classList[functionName]('d-none')
   }
+}
 
-  function fnIntegracionMesDespido (salary, monthWorkingDays) {
-      return (salary / 30 * (30 - monthWorkingDays));
-  }
-
-
-  function resignationLiquidation (yearWorkingDays, semesterWorkingDays, vacationDays, salari) {
-    const aguinaldoProporcional = fnAguinaldoProporcional(salari, semesterWorkingDays);
-    const vacacionesNoGozadas = fnVacacionesNoGozadas(salari, vacationDays, yearWorkingDays);
-    const aguinaldoOverVacaciones = vacacionesNoGozadas / 12;
-  
-    return {
-      resultAsObj: {
-        aguinaldoProporcional: aguinaldoProporcional,
-        vacacionesNoGozadas: vacacionesNoGozadas,
-        aguinaldoOverVacaciones: aguinaldoOverVacaciones
-      },
-      resultAsArr: [
-        aguinaldoProporcional,
-        vacacionesNoGozadas,
-        aguinaldoOverVacaciones
-      ]
-    }
-  }
-
-  function fireLiquidation (yearWorkingDays, semesterWorkingDays, vacationDays, salari, monthWorkingDays) {
-      const aguinaldoProporcional = fnAguinaldoProporcional(salari, semesterWorkingDays);
-      const vacacionesNoGozadas = fnVacacionesNoGozadas(salari, vacationDays, yearWorkingDays);
-      const aguinaldoOverVacaciones = vacacionesNoGozadas / 12;
-      const integracionMesDespido = fnIntegracionMesDespido(salari, monthWorkingDays);
-      const mesPreaviso = fnMesPreaviso(salari, monthWorkingDays)
+function onFinalSubmit () {
+  console.log(userInput)
+}
 
 
+// ------------------------------------------
 
-  }
+let currentStep = 1
+const totalSteps = 5
+
+const userInput = [
+  null, // inDate
+  null, // outDate
+  null, // salary
+  null, // salary
+  null, // pendingVacations
+]
+const formToSend = document.getElementById('form-1');
+
+formToSend.addEventListener('submit', onSubmit)
+updateInputVisibiliy()
